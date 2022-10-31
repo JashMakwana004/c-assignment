ba2dec76559699ef61b050eb5a2be14d65ced85d
JashMakwana004 <111413935+JashMakwana004@users.noreply.github.com>
2022-10-31 17:05:42 +0530

    Delete introduction.cpp

--- introduction.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-	printf("Your name : Jash Makwana");
-	printf("\nYour age : 18");
-	printf("\nYour bithdate : 18/11/2004");
-	printf("\nYour address : dharti status flat");
-}
